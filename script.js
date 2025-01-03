5ccd91fca673774703b2700d9473655bdb9fc2ec
Ankita pawar <ankita.k.pawar1906@gmail.com>
2025-01-03 15:41:15 +0530

    added login

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var res;
+var userDetails;
 function getData() {
     fetch('https://jsonblob.com/api/jsonBlob/1319247450992730112')
         .then(response => {
@@ -11,26 +12,6 @@ function getData() {
         .then(data => {
             // Process the response data here
             res = data;
-            populateTable(res.tradeHistory);
-            totalPnl = calculateTotalPnL(res.tradeHistory);
-            document.getElementById("pnl").innerHTML = totalPnl + " INR";
-            document.getElementById("capital").innerHTML = res?.cardData?.capital + " INR";
-            res.cardData.balance = res?.cardData?.capital + (calculateTotalPnL(res.tradeHistory));
-            document.getElementById("balance").innerHTML = res?.cardData?.capital + (calculateTotalPnL(res.tradeHistory)) + " INR";
-            let dir = `${res.cardData.balance < res.cardData.capital ? "-" : "+"}`;
-            document.getElementById("gain").innerHTML = dir + (res?.cardData?.balance / res?.cardData?.capital) * 100 + " %";
-            if (dir === "+") {
-                document.getElementById("gain").classList.add("gain")
-            } else {
-                document.getElementById("gain").classList.add("loss")
-            }
-
-            if (totalPnl > 0) {
-                document.getElementById("pnl").classList.add("gain")
-            } else {
-                document.getElementById("pnl").classList.add("loss")
-            }
-
         })
         .catch(error => {
             // Handle any errors here
@@ -52,7 +33,6 @@ function populateTable(data) {
         ];
 
         cells.forEach((cellText, i) => {
-            console.log("celltext", i, cellText)
             const td = document.createElement('td');
             if (i === 3) {
                 if (item.direction === "+") {
@@ -75,4 +55,47 @@ function calculateTotalPnL(data) {
             ? sum + (item.pnl * 0.25)
             : sum - item.pnl / 2;
     }, 0);
+}
+
+function login(un, pwd) {
+    if (validateCredentials(res, un, pwd) !== null) {
+        document.getElementById("login").style = "display:none;"
+        document.getElementById("dashboard").style = "display:block;"
+        document.getElementById("loginError").style = "display:none;"
+
+        userDetails = validateCredentials(res, un, pwd);
+
+        document.getElementById("username").innerHTML = `(${userDetails.user.name})`;
+        populateTable(userDetails.tradeHistory);
+        totalPnl = calculateTotalPnL(userDetails.tradeHistory);
+        document.getElementById("pnl").innerHTML = totalPnl + " INR";
+        document.getElementById("capital").innerHTML = userDetails?.cardData?.capital + " INR";
+        userDetails.cardData.balance = userDetails?.cardData?.capital + (calculateTotalPnL(userDetails.tradeHistory));
+        document.getElementById("balance").innerHTML = userDetails?.cardData?.capital + (calculateTotalPnL(userDetails.tradeHistory)) + " INR";
+        let dir = `${userDetails.cardData.balance < userDetails.cardData.capital ? "-" : "+"}`;
+        document.getElementById("gain").innerHTML = dir + (userDetails?.cardData?.balance / userDetails?.cardData?.capital) * 100 + " %";
+        if (dir === "+") {
+            document.getElementById("gain").classList.add("gain")
+        } else {
+            document.getElementById("gain").classList.add("loss")
+        }
+
+        if (totalPnl > 0) {
+            document.getElementById("pnl").classList.add("gain")
+        } else {
+            document.getElementById("pnl").classList.add("loss")
+        }
+    } else {
+        document.getElementById("dashboard").style = "display:none;"
+        document.getElementById("loginError").style = "display:block;"
+    }
+
+
+}
+
+function validateCredentials(data, username, password) {
+    const matchedUser = data.find(item =>
+        item.user.username === username && item.user.pwd === password
+    );
+    return matchedUser || null; // Return the matched user or null if no match
 }
